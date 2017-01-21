589fb0db71784715ffe17895107a49737770690d
Abhinav Chauhan <alfa.abhi1996@gmail.com>
2017-01-21 13:41:40 +0530

    unnecessary data deleted

--- untitled4/settings.py
+++ untitled4/settings.py
@@ -93,7 +93,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
